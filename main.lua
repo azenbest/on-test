d64d66e1f5cc3b9f152b6e7629f4cbf19a1f3a3d
azenbest <melvyncalcoen1@gmail.com>
2025-02-12 23:09:10 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -85,4 +85,109 @@ local Input = Tabs.Main:CreateInput("SizeChanger", {
 			game:GetService("ReplicatedStorage").rEvents.changeSpeedSizeRemote:InvokeServer("changeSize", tonumber(selectedSize))
 		end
 	end
+}}
+
+local Toggle = Tabs.Main:CreateToggle("AutoSize", {
+	Title = "Auto Set Size",
+	Description = "Auto Set ur Choosed Size",
+	Default = false,
+	Callback = function(Value)
+		_G.AutoSize = Value
+		while _G.AutoSize do
+			game:GetService("ReplicatedStorage").rEvents.changeSpeedSizeRemote:InvokeServer("changeSize", tonumber(selectedSize))
+			task.wait(0.1)
+		end
+	end
+})
+
+local selectedSpeed = "125"
+local Input = Tabs.Main:CreateInput("SpeedChanger", {
+	Title = "Speed Changer",
+	Description = "Enter Speed",
+	Default = "125",
+	Placeholder = "Enter Speed",
+	Numeric = true,
+	Finished = true,
+	Callback = function(Value)
+		selectedSpeed = Value
+		if _G.AutoSpeed then
+			if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
+				game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = tonumber(selectedSpeed)
+			end
+		end
+	end
+})
+
+local Toggle = Tabs.Main:CreateToggle("AutoSpeed", {
+	Title = "Auto Set Speed",
+	Description = "Auto Set ur Choosed Speed",
+	Default = false,
+	Callback = function(Value)
+		_G.AutoSpeed = Value
+		while _G.AutoSpeed do
+			if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
+				game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = tonumber(selectedSpeed)
+			end
+			task.wait()
+		end
+	end
+})
+
+game.Players.LocalPlayer.CharacterAdded:Connect(function(char)
+	if _G.AutoSpeed then
+		local humanoid = char:WaitForChild("Humanoid")
+		humanoid.WalkSpeed = tonumber(selectedSpeed)
+	end
+end)
+
+Tabs.Main:CreateButton{
+	Title = "Free AutoLift Gamepass",
+	Callback = function()
+		local gamepassFolder = game:GetService("ReplicatedStorage").gamepassIds
+		local player = game:GetService("Players").LocalPlayer
+		for _, gamepass in pairs(gamepassFolder:GetChildren()) do
+			local value = Instance.new("IntValue")
+			value.Name = gamepass.Name
+			value.Value = gamepass.Value
+			value.Parent = player.ownedGamepasses
+		end
+	end
+}
+
+local Toggle = Tabs.Main:CreateToggle("WalkOnWater", {
+	Title = "Walk on Water",
+	Description = "",
+	Default = false,
+	Callback = function(Value)
+		if Value then
+			createParts()
+		else
+			makePartsWalkthrough()
+		end
+	end
+})
+
+local Toggle = Tabs.AutoFarm:CreateToggle("Weight", {
+	Title = "Auto Weight",
+	Description = "Auto Lift Weight",
+	Default = false,
+	Callback = function(Value)
+		_G.AutoWeight = Value
+		if Value then
+			local weightTool = game.Players.LocalPlayer.Backpack:FindFirstChild("Weight")
+			if weightTool then
+				game.Players.LocalPlayer.Character.Humanoid:EquipTool(weightTool)
+			end
+		else
+			local character = game.Players.LocalPlayer.Character
+			local equipped = character:FindFirstChild("Weight")
+			if equipped then
+				equipped.Parent = game.Players.LocalPlayer.Backpack
+			end
+		end
+		while _G.AutoWeight do
+			game:GetService("Players").LocalPlayer.muscleEvent:FireServer("rep")
+			task.wait(0)
+		end
+	end
 })

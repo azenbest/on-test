80294e78084b3f8ebe57e778941e55926443e985
azenbest <melvyncalcoen1@gmail.com>
2025-02-14 11:29:41 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -79,38 +79,70 @@ local Window = Library:CreateWindow{
 }
 
 local Tabs = {
-    Main = Window:CreateTab{ Title = "Main", Icon = "phosphor-house-bold" },
-    AutoBuy = Window:CreateTab{ Title = "Auto Buy", Icon = "phosphor-shopping-cart-bold" },
-    AutoStuff = Window:CreateTab{ Title = "Auto Stuff", Icon = "phosphor-robot-bold" },
-    AutoFarm = Window:CreateTab{ Title = "Auto Farm", Icon = "phosphor-robot-bold" },
-    Rebirth = Window:CreateTab{ Title = "Rebirth", Icon = "phosphor-arrows-clockwise-bold" },
-    Killer = Window:CreateTab{ Title = "Killer", Icon = "phosphor-sword-bold" },
-    Crystals = Window:CreateTab{ Title = "Crystals", Icon = "phosphor-diamond-bold" },
-    Teleport = Window:CreateTab{ Title = "Teleport", Icon = "phosphor-dog-bold" },
-    Stats = Window:CreateTab{ Title = "Stats", Icon = "phosphor-sparkle-bold" },
-    Misc = Window:CreateTab{ Title = "Misc", Icon = "phosphor-map-pin-bold" },
-    Settings = Window:CreateTab{ Title = "Settings", Icon = "phosphor-sliders-bold" }
+	Main = Window:CreateTab{
+		Title = "Main",
+		Icon = "phosphor-house-bold"
+	},
+	AutoBuy = Window:CreateTab{
+		Title = "Auto Buy",
+		Icon = "phosphor-shopping-cart-bold"
+	},
+	AutoStuff = Window:CreateTab{
+		Title = "Auto Stuff",
+		Icon = "phosphor-robot-bold"
+	},
+	AutoFarm = Window:CreateTab{
+		Title = "Auto Farm",
+		Icon = "phosphor-robot-bold"
+	},
+	Rebirth = Window:CreateTab{
+		Title = "Rebirth",
+		Icon = "phosphor-arrows-clockwise-bold"
+	},
+	Killer = Window:CreateTab{
+		Title = "Killer",
+		Icon = "phosphor-sword-bold"
+	},
+	Crystals = Window:CreateTab{
+		Title = "Crystals",
+		Icon = "phosphor-diamond-bold"
+	},
+	Teleport = Window:CreateTab{
+		Title = "Teleport",
+		Icon = "phosphor-dog-bold"
+	},
+	Stats = Window:CreateTab{
+		Title = "Stats",
+		Icon = "phosphor-sparkle-bold"
+	},
+	Misc = Window:CreateTab{
+		Title = "Misc",
+		Icon = "phosphor-map-pin-bold"
+	},
+	Settings = Window:CreateTab{
+		Title = "Settings",
+		Icon = "phosphor-sliders-bold"
+	}
 }
 
 local Options = Library.Options  
-
 local MainSection = Tabs.Main:CreateSection("Basic Controls")
 local selectedSize = "2"
 
-local Input = MainSection:CreateInput{
-    Title = "Size Changer",
-    Description = "Enter Size",
-    Default = "2",
-    Placeholder = "Type here...",
-    Numeric = true,
-    Finished = true,
-    Callback = function(Value)
-        selectedSize = Value
-        if _G.AutoSize then
-            game:GetService("ReplicatedStorage").rEvents.changeSpeedSizeRemote:InvokeServer("changeSize", tonumber(selectedSize))
-        end
-    end
-}
+local Input = Tabs.Main:CreateInput("SizeChanger", {
+	Title = "Size Changer",
+	Description = "Enter Size",
+	Default = "2",
+	Placeholder = "Type here...",
+	Numeric = true,
+	Finished = true,
+	Callback = function(Value)
+		selectedSize = Value
+		if _G.AutoSize then
+			game:GetService("ReplicatedStorage").rEvents.changeSpeedSizeRemote:InvokeServer("changeSize", tonumber(selectedSize))
+		end
+	end
+})
 
 
 local Toggle = Tabs.Main:CreateToggle("AutoSize", {
